ffa149d2b5f371e7ec8a5b9f1b627d3e704537a3
HannahLowery <163455157+HannahLowery@users.noreply.github.com>
2026-03-25 18:20:39 -0500

    starter code

---
+++ Backend/src/main/java/com/project/uniride/UnirideApplication.java
@@ -0,0 +1,13 @@
+package com.project.uniride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnirideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnirideApplication.class, args);
+	}
+
+}
