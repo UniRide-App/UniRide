86a0627b47a9570154a8d5314f6c38f9d42a3d65
Jeremiah McKeey <jmcke24@lsu.edu>
2026-04-15 16:33:15 -0500

    Basic authentication system
    Initial basic authentication system, will work on connecting this to the student class so the authenticated student username and password isn't hard coded

---
+++ Backend/src/main/java/com/project/uniride/TestController.java
@@ -0,0 +1,16 @@
+package com.project.uniride;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/public/hello")
+    public String publicHello(){
+        return "Public endpoint";
+    }
+    @GetMapping("/secure/hello")
+    public String secureHello(){
+        return "You have been authenticated!";
+    }
+}
