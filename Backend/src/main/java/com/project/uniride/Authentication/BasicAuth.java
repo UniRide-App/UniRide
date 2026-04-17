8342985d61ec04c53c68458fd91413125b4907b6
HannahLowery <163455157+HannahLowery@users.noreply.github.com>
2026-04-16 20:56:10 -0500

    moved into folder

--- Backend/src/main/java/com/project/uniride/Authentication/BasicAuth.java
+++ Backend/src/main/java/com/project/uniride/Authentication/BasicAuth.java
@@ -1,11 +1,12 @@
-package com.project.uniride;
+package com.project.uniride.Authentication;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.*;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
--- Backend/src/main/java/com/project/uniride/Authentication/TestController.java
+++ Backend/src/main/java/com/project/uniride/Authentication/TestController.java
@@ -1,4 +1,4 @@
-package com.project.uniride;
+package com.project.uniride.Authentication;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
