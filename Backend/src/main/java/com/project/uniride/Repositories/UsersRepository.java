7f74890b5c7c6cc8aa44f8fcf80add224f9dbc1e
HannahLowery <163455157+HannahLowery@users.noreply.github.com>
2026-04-16 23:44:27 -0500

    fixed Entities and Repositories

--- Backend/src/main/java/com/project/uniride/Repositories/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.uniride.Repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.project.uniride.Entities.UsersEntity;
-
-public interface UsersRepository extends CrudRepository<UsersEntity, Long>{
-    
-}
